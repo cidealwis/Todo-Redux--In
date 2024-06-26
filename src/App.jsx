e74fd61cc37b02abfebf2d4ee40f92590624daf6
Chathun <Chathunishara4@gmail.com>
2024-06-26 14:38:16 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-
-import './App.css'
+import Todo from './view/Todo'
 
 function App() {
 
 
   return (
     <>
-      <h1>Hello Developer</h1>
+      <Todo/>
     </>
   )
 }
